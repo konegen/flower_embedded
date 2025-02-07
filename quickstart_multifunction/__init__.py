2e6fdeb5cf58934160c51e5fe6c20de36632013c
konegen <daniel.konegen@web.de>
2025-02-07 13:02:27 +0100

    Restructured project, client and strategy classes outsourced

--- quickstart_multifunction/__init__.py
+++ quickstart_multifunction/__init__.py
@@ -1,1 +1,19 @@
-"""pandas_example: A Flower / Pandas app."""
+import sys
+import os
+
+# Füge das Elternverzeichnis hinzu, damit das Modulverzeichnis direkt erkannt wird
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from . import client
+from . import strategy
+from .task import load_model, load_data
+from .utils import evaluate_config, fit_config
+
+__all__ = [
+    "client",
+    "strategy",
+    "load_model",
+    "load_data",
+    "evaluate_config",
+    "fit_config",
+]
--- quickstart_multifunction/client_app.py
+++ quickstart_multifunction/client_app.py
@@ -1,65 +1,15 @@
-import json
 import warnings
-from logging import INFO, WARNING
+from logging import INFO
 
-import numpy as np
 from flwr.client import ClientApp
-from flwr.common import Context, Message, MetricsRecord, RecordSet
 from flwr.common.logger import log
+from flwr.common import Context
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
+from task import load_data, load_model
 
-from flwr.client import NumPyClient
-from flwr.common import Context
-
-from quickstart_multifunction.task import load_data, load_model
-
-
-class FlowerClientStatistic(NumPyClient):
-    def __init__(self, data):
-        self.x_train, self.y_train, self.x_test, self.y_test = data
-
-    def evaluate(self, parameters, config):
-        metrics = {}
-
-        # Compute some statistics for each column in the dataframe
-        for feature_name in self.x_train.columns:
-            for metric in json.loads(config["metrics"]):
-                if hasattr(self.x_train[feature_name], metric):
-                    # Falls die Metrik existiert, berechnen
-                    metrics[f"{feature_name}_{metric}"] = getattr(
-                        self.x_train[feature_name], metric
-                    )()
-                else:
-                    # Falls die Metrik nicht existiert, Warnung ausgeben und None setzen
-                    log(WARNING, f"Metric '{metric}' is not known")
-                    metrics[f"{feature_name}_{metric}"] = None
-        return float(0.0), len(self.x_train), metrics
-
-
-class FlowerClientTrain(NumPyClient):
-    def __init__(self, model, data):
-        self.model = model
-        self.x_train, self.y_train, self.x_test, self.y_test = data
-
-    def fit(self, parameters, config):
-        self.model.set_weights(parameters)
-        log(INFO, f'config["local_epochs"]: {config["local_epochs"]}')
-        log(INFO, f'config["batch_size"]: {config["batch_size"]}')
-        self.model.fit(
-            self.x_train,
-            self.y_train,
-            epochs=config["local_epochs"],
-            batch_size=config["batch_size"],
-            verbose=config["verbose"],
-        )
-        return self.model.get_weights(), len(self.x_train), {}
-
-    def evaluate(self, parameters, config):
-        self.model.set_weights(parameters)
-        loss, accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-        return loss, len(self.x_test), {"accuracy": accuracy}
+from client import FlowerClientStatistic, FlowerClientTrain
 
 
 def client_fn(context: Context):
