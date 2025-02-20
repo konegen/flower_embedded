dd615d7ccde1896a2b7d841693efd7447b4baebf
konegen <daniel.konegen@web.de>
2025-02-20 15:23:55 +0100

    Remove files

--- quickstart_multifunction/client/__init__.py
+++
@@ -1,7 +0,0 @@
-from .flower_client_statistic import FlowerClientStatistic
-from .flower_client_train import FlowerClientTrain
-
-__all__ = [
-    "FlowerClientStatistic",
-    "FlowerClientTrain",
-]
--- quickstart_multifunction/strategy/__init__.py
+++
@@ -1,7 +0,0 @@
-from .aggregate_calculated_statistics import AggregateCalculatedStatistics
-from .aggregate_custom_metric_strategy import AggregateCustomMetricStrategy
-
-__all__ = [
-    "AggregateCalculatedStatistics",
-    "AggregateCustomMetricStrategy",
-]
